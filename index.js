181160ee79053ae4ee5cff300453366e7fbbdb38
Claire Kodia <claire.kodia@gmail.com>
2020-06-10 16:54:46 +0200

    Addedthe auth route to the index.js file

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+
 const app = express()
 const { port } = require('./config')
+const routes = require('./src/routes/index')
+
+app.use('/auth', routes.Auth)
 
 app.listen(port, () => {
   console.log(`server is listening on port ${port}`)
