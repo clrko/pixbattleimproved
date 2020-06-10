269de61fd5f917bfa3f5f8af77278eb555510d58
Claire Kodia <claire.kodia@gmail.com>
2020-06-10 19:44:51 +0200

    Saved

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -7,7 +7,6 @@ const router = express.Router()
 router.get('/', (req, res) => {
   const sql = 'SELECT * FROM user WHERE user_id = ?'
   const values = 1
-
   connection.query(sql, values, (err, res) => {
     if (err) throw err
     console.log('res est', res)
