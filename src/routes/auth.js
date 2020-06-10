0242b296f01a80c828405443efe0d16d4320e453
Claire Kodia <claire.kodia@gmail.com>
2020-06-10 19:43:37 +0200

    Created a sample auth route to do a simple test query to mysql

---
+++ src/routes/auth.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const connection = require('../helper/db')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  const sql = 'SELECT * FROM user WHERE user_id = ?'
+  const values = 1
+
+  connection.query(sql, values, (err, res) => {
+    if (err) throw err
+    console.log('res est', res)
+  })
+})
+
+module.exports = router
