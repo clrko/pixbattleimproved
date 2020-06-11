0c47717e7b7dabf71e28ca8fc735eadfbf756302
Lola <lola.donval@gmail.com>
2020-06-11 17:07:12 +0200

    add JWTSECRET

--- config.js
+++ config.js
@@ -6,5 +6,6 @@ module.exports = {
   dbHost: process.env.DB_HOST,
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
-  dbDatabase: process.env.DB_DATABASE
+  dbDatabase: process.env.DB_DATABASE,
+  jwtSecret: process.env.JWTSECRET
 }
