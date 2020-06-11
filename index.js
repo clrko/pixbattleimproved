335a1f8663b010355179c8a5a2d19b957283deb3
Lola <lola.donval@gmail.com>
2020-06-11 11:29:15 +0200

    import all needed packages

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const express = require('express')
+const morgan = require('morgan')
+
+const pixBattle = require('./src/routes/register.js')
+
 const app = express()
 const { port } = require('./config')
 
+app.use(morgan('dev'))
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use('/pixBattle', pixBattle)
+
+app.get('/', (req, res) => {
+  res.send('I am here /')
+})
+
 app.listen(port, () => {
-  console.log(`server is listening on port ${port}`)
+  console.log('server is listening on port 8080')
 })
