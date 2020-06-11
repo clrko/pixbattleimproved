90f7cae4fc142b119272dd2fa5099ec8f125edf8
Claire Kodia <claire.kodia@gmail.com>
2020-06-11 15:47:50 +0200

    Added the parser lines

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 
-const app = express()
 const { port } = require('./config')
 const routes = require('./src/routes/index')
 
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use('/auth', routes.Auth)
 
 app.listen(port, () => {
