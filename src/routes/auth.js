c13852fe6f4b2d91ed1c6d9221bc947022182d79
Claire Kodia <claire.kodia@gmail.com>
2020-06-11 15:54:21 +0200

    Working version of the auth route with the token and response

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,15 +1,44 @@
 const express = require('express')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 const connection = require('../helper/db')
+const { jwtSecret } = require('../../config')
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  const sql = 'SELECT * FROM user WHERE user_id = ?'
-  const values = 1
-  connection.query(sql, values, (err, res) => {
+  const sql = 'SELECT u.user_id, u.username, u.email, u.password, a.avatar_url FROM user AS u JOIN avatar AS a ON u.avatar_id = a.avatar_id WHERE email = ?'
+  const values = [
+    req.body.email
+  ]
+  connection.query(sql, values, (err, result) => {
     if (err) throw err
-    console.log('res est', res)
+    if (result.length === 0) {
+      return res.status(401).send('The password or username is wrong')
+    }
+    console.log(result)
+    const myPlaintextPassword = req.body.password
+    const userId = result[0].user_id
+    const username = result[0].username
+    const avatar = result[0].avatar_url
+
+    bcrypt.compare(myPlaintextPassword, result[0].password, (err, result) => {
+      if (err) throw err
+      if (result) {
+        const tokenUserInfo = {
+          userId: userId,
+          username: username,
+          avatar: avatar
+        }
+        const token = jwt.sign(tokenUserInfo, jwtSecret)
+        res.header('Access-Control-Expose-Headers', 'x-access-token')
+        res.set('x-access-token', token)
+        return res.status(200).send(tokenUserInfo)
+      } else {
+        return res.status(401).send('The password or username is wrong')
+      }
+    })
   })
 })
 
