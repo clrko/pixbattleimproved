a59660d492c6c721213a56e1824431f6e5f4521e
Lola <lola.donval@gmail.com>
2020-06-30 12:27:21 +0200

    create routes for the upload step battle

---
+++ src/routes/battle-post.js
@@ -0,0 +1,83 @@
+const express = require('express')
+
+const checkToken = require('../helper/checkToken')
+const connection = require('../helper/db')
+
+const router = express.Router()
+
+router.get('/', checkToken, (req, res) => {
+  const sqlGroupName = 'SELECT group_name FROM `group` WHERE group_id = ?'
+  const valueGroupId = [
+    req.body.groupId
+  ]
+  connection.query(sqlGroupName, valueGroupId, (err, result) => {
+    if (err) throw err
+    const groupName = {
+      groupName: result[0].group_name
+    }
+    const sqlBattleInfos = 'SELECT t.theme_name, b.deadline, r.rule_name FROM theme AS t JOIN battle AS b ON b.theme_id = t.theme_id JOIN battle_rule AS br ON br.battle_id = b.battle_id JOIN rule AS r ON r.rule_id = br.rule_id WHERE b.battle_id = ?'
+    const valueBattleId = [
+      req.body.battleId
+    ]
+    connection.query(sqlBattleInfos, valueBattleId, (err, battleInfos) => {
+      if (err) throw err
+      const sqlBattleMembers = 'SELECT u.username, u.user_id, a.avatar_url FROM avatar AS a JOIN user AS u ON u.avatar_id = a.avatar_id JOIN user_battle AS ub ON ub.user_id = u.user_id WHERE ub.battle_id = ?'
+      connection.query(sqlBattleMembers, valueBattleId, (err, battleMembers) => {
+        if (err) throw err
+        const sqlMemberStatus = 'SELECT u.user_id FROM user AS u JOIN photo AS p ON p.user_id = u.user_id WHERE p.battle_id = ?'
+        connection.query(sqlMemberStatus, valueBattleId, (err, battleMemberStatus) => {
+          if (err) throw err
+          const infos = {
+            groupName,
+            battleInfos,
+            battleMembers,
+            battleMemberStatus
+          }
+          res.status(200).send(infos)
+        })
+      })
+    })
+  })
+})
+
+router.post('/', checkToken, (req, res) => {
+  const sqlInsertPhoto = 'INSERT INTO photo (photo_url, create_date, user_id, battle_id, group_id) VALUES (?, NOW(), ?, ?, ?)'
+  const valuesInsertPhoto = [
+    req.body.photoUrl,
+    req.body.userId,
+    req.body.battleId,
+    req.body.groupId
+  ]
+  connection.query(sqlInsertPhoto, valuesInsertPhoto, (err, photoRes) => {
+    if (err) throw err
+    const photoId = {
+      photoId: photoRes.insertId
+    }
+    res.status(200).send(photoId)
+  })
+})
+
+router.put('/', checkToken, (req, res) => {
+  const sqlUpdatePhoto = 'UPDATE photo SET photo_url = ? WHERE photo_id = ?'
+  const valuesUpdatePhoto = [
+    req.body.photoUrl,
+    req.body.photoId
+  ]
+  connection.query(sqlUpdatePhoto, valuesUpdatePhoto, err => {
+    if (err) throw err
+    res.sendStatus(200)
+  })
+})
+
+router.delete('/', checkToken, (req, res) => {
+  const sqlDeletePhoto = 'DELETE FROM photo WHERE photo_id = ?'
+  const valueDeletePhoto = [
+    req.body.photoId
+  ]
+  connection.query(sqlDeletePhoto, valueDeletePhoto, err => {
+    if (err) throw err
+    res.sendStatus(200)
+  })
+})
+
+module.exports = router
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const Auth = require('./auth')
+const BattlePost = require('./battle-post')
 const Group = require('./group')
 const GroupCreation = require('./group-creation')
 const Register = require('./register')
 
 module.exports = {
   Auth,
+  BattlePost,
   Group,
   GroupCreation,
   Register
