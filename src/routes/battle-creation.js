f00c88e4c75d4c7701a66eb1bc7066c6fa4e829a
Claire Kodia <claire.kodia@gmail.com>
2020-07-01 15:33:55 +0200

    Added the checktoken method to the battle creation file

--- src/routes/battle-creation.js
+++ src/routes/battle-creation.js
@@ -1,23 +1,22 @@
 const express = require('express')
-
-/* const checkToken = require('../helper/checkToken') */
+const checkToken = require('../helper/checkToken')
 const connection = require('../helper/db')
 
 const router = express.Router()
-// ajouter checktoken
-router.get('/themes', (req, res) => {
+
+router.get('/themes', checkToken, (req, res) => {
   const sql = 'SELECT * FROM theme ORDER BY RAND() LIMIT 10;'
   connection.query(sql, (err, themeList) => {
     if (err) throw err
-    return res.status(200).send(themeList)
+    return res.status(201).send(themeList)
   })
 })
 
-router.get('/rules', (req, res) => {
+router.get('/rules', checkToken, (req, res) => {
   const sql = 'SELECT * FROM rule;'
   connection.query(sql, (err, ruleList) => {
     if (err) throw err
-    return res.status(200).send(ruleList)
+    return res.status(201).send(ruleList)
   })
 })
 
