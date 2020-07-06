4a0c44cf8782d179716fe4ec068a3742b5dc8498
Maxime Poitoux <poitouxmaxime@hotmail.fr>
2020-07-06 11:56:37 +0200

    request finished

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -5,7 +5,6 @@ const connection = require('../helper/db')
 
 const router = express.Router()
 
-// RECUPERER LES VICTOIRES D'UN USER
 router.get('/', (req, res) => {
   const sqlUserInfos = 'SELECT u.username, a.avatar_url, b.winner_user_id, count(b.winner_user_id) AS victories FROM avatar AS a JOIN user AS u ON a.avatar_id = u.avatar_id JOIN battle AS b ON b.winner_user_id = u.user_id JOIN user_battle AS ub ON ub.user_id = u.user_id WHERE u.user_id = ? GROUP BY b.winner_user_id'
   const valueUserId = [req.body.userId]
@@ -15,41 +14,50 @@ router.get('/', (req, res) => {
       const sqlUserInfosEmpty = 'SELECT u.username, a.avatar_url FROM avatar AS a JOIN user AS u ON a.avatar_id = u.avatar_id WHERE u.user_id = ?'
       connection.query(sqlUserInfosEmpty, valueUserId, (err, userInfos) => {
         if (err) throw err
-        return res.status(200).send(userInfos[0])
+        const infos = userInfos[0]
+        const sqlNumberPhotosUser = 'SELECT count(*) AS nb_photos FROM photo WHERE user_id = ?'
+        connection.query(sqlNumberPhotosUser, valueUserId, (err, nbPhotos) => {
+          if (err) throw err
+          const sqlNumberGroupsUser = 'SELECT count(*) AS nb_groups FROM user_group WHERE user_id = ?'
+          connection.query(sqlNumberGroupsUser, valueUserId, (err, nbGroups) => {
+            if (err) throw err
+            const sqlNumberBattlesUser = 'SELECT count(*) AS nb_battles FROM user_battle WHERE user_id = ?'
+            connection.query(sqlNumberBattlesUser, valueUserId, (err, nbBattles) => {
+              if (err) throw err
+              const allInfos = {
+                infos,
+                nbPhotos,
+                nbGroups,
+                nbBattles
+              }
+              res.status(200).send(allInfos)
+            })
+          })
+        })
       })
-    } else {
-      return res.status(200).send(userInfos[0])
     }
-  })
-})
-
-// RECUPERER LE NOMBRE DE PHOTOS D'UN USER
-router.get('/nbPhotos', (req, res) => {
-  const sqlNumberPhotosUser = 'SELECT count(*) AS nb_photos FROM photo WHERE user_id = ?'
-  const valueUserId = [req.body.userId]
-  connection.query(sqlNumberPhotosUser, valueUserId, (err, nbPhotos) => {
-    if (err) throw err
-    res.status(200).send(nbPhotos)
-  })
-})
-
-// RECUPERER LE NOMBRE DE GROUPE D'UN USER
-router.get('/nbGroups', (req, res) => {
-  const sqlNumberGroupsUser = 'SELECT count(*) AS nb_groups FROM user_group WHERE user_id = ?'
-  const valueUserId = [req.body.userId]
-  connection.query(sqlNumberGroupsUser, valueUserId, (err, nbGroups) => {
-    if (err) throw err
-    res.status(200).send(nbGroups)
-  })
-})
-
-// RECUPERER LE NOMBRE DE BATTLES
-router.get('/nbBattles', (req, res) => {
-  const sqlNumberBattlesUser = 'SELECT count(*) AS nb_battles FROM user_battle WHERE user_id = 2'
-  const valueUserId = [req.body.userId]
-  connection.query(sqlNumberBattlesUser, valueUserId, (err, nbBattles) => {
-    if (err) throw err
-    res.status(200).send(nbBattles)
+    if (userInfos[0]) {
+      const infos = userInfos[0]
+      const sqlNumberPhotosUser = 'SELECT count(*) AS nb_photos FROM photo WHERE user_id = ?'
+      connection.query(sqlNumberPhotosUser, valueUserId, (err, nbPhotos) => {
+        if (err) throw err
+        const sqlNumberGroupsUser = 'SELECT count(*) AS nb_groups FROM user_group WHERE user_id = ?'
+        connection.query(sqlNumberGroupsUser, valueUserId, (err, nbGroups) => {
+          if (err) throw err
+          const sqlNumberBattlesUser = 'SELECT count(*) AS nb_battles FROM user_battle WHERE user_id = ?'
+          connection.query(sqlNumberBattlesUser, valueUserId, (err, nbBattles) => {
+            if (err) throw err
+            const allInfos = {
+              infos,
+              nbPhotos,
+              nbGroups,
+              nbBattles
+            }
+            res.status(200).send(allInfos)
+          })
+        })
+      })
+    }
   })
 })
 
