3dc991e590d08d5091e8cfafb29940be54878a91
Maxime Poitoux <poitouxmaxime@hotmail.fr>
2020-07-06 12:43:34 +0200

    updates

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,13 +1,14 @@
 const express = require('express')
 
-// const checkToken = require('../helper/checkToken')
+const checkToken = require('../helper/checkToken')
 const connection = require('../helper/db')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
+router.get('/', checkToken, (req, res) => {
   const sqlUserInfos = 'SELECT u.username, a.avatar_url, b.winner_user_id, count(b.winner_user_id) AS victories FROM avatar AS a JOIN user AS u ON a.avatar_id = u.avatar_id JOIN battle AS b ON b.winner_user_id = u.user_id JOIN user_battle AS ub ON ub.user_id = u.user_id WHERE u.user_id = ? GROUP BY b.winner_user_id'
-  const valueUserId = [req.body.userId]
+  const valueUserId = [req.user.userId]
+  console.log(valueUserId)
   connection.query(sqlUserInfos, valueUserId, (err, userInfos) => {
     if (err) throw err
     if (!userInfos[0]) {
