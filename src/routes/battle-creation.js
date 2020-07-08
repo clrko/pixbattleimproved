b88da99e273aaede51fb73cc68cba21bc6e16f08
Claire Kodia <claire.kodia@gmail.com>
2020-07-08 10:59:44 +0200

    last working version of the battle creation routes

--- src/routes/battle-creation.js
+++ src/routes/battle-creation.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const checkToken = require('../helper/checkToken')
+/* const checkToken = require('../helper/checkToken') */
 const connection = require('../helper/db')
 
 const router = express.Router()
 
-router.get('/themes', checkToken, (req, res) => {
+router.get('/themes', /* checkToken, */ (req, res) => {
   const sql = 'SELECT * FROM theme ORDER BY RAND() LIMIT 10;'
   connection.query(sql, (err, themeList) => {
     if (err) throw err
@@ -12,7 +12,7 @@ router.get('/themes', checkToken, (req, res) => {
   })
 })
 
-router.get('/rules', checkToken, (req, res) => {
+router.get('/rules', /* checkToken, */ (req, res) => {
   const sql = 'SELECT * FROM rule;'
   connection.query(sql, (err, ruleList) => {
     if (err) throw err
@@ -21,7 +21,7 @@ router.get('/rules', checkToken, (req, res) => {
 })
 
 router.post('/', /* checkToken, */ (req, res) => {
-  const userId = req.body.userId
+  const userId = 1
   const sql = 'INSERT INTO battle (deadline, group_id, theme_id, admin_user_id) VALUES (?, ?, ?, ?)'
   const value = [
     req.body.deadline,
@@ -32,10 +32,10 @@ router.post('/', /* checkToken, */ (req, res) => {
   connection.query(sql, value, (err, battleCreationResult) => {
     if (err) throw err
     const sqlBattleRule = 'INSERT INTO battle_rule VALUES ?'
-    const insertBattleRulesValues = req.body.allRules.map(rule => [battleCreationResult.insertId, rule.rule_id])
+    const insertBattleRulesValues = req.body.rulesId.map(rule => [battleCreationResult.insertId, rule])
     connection.query(sqlBattleRule, [insertBattleRulesValues], err => {
       if (err) throw err
-      return res.sendStatus(200)
+      return res.sendStatus(201)
     })
     const sqlUserBattle = 'INSERT INTO user_battle VALUES ?'
     const userBattleValues = [
@@ -44,7 +44,7 @@ router.post('/', /* checkToken, */ (req, res) => {
     ]
     connection.query(sqlUserBattle, userBattleValues, err => {
       if (err) throw err
-      return res.sendStatus(200)
+      return res.sendStatus(201)
     })
   })
 })
