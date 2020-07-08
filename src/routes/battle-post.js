517fb3da7ca321693b4348a199b99cce10743275
Aristide-O <heavenly_7@hotmail.fr>
2020-07-08 15:53:25 +0200

    deleted some picture

--- src/routes/battle-post.js
+++ src/routes/battle-post.js
@@ -8,7 +8,7 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, '/Users/ariomega/Desktop/Pix_Battle/paris-react-2003-pjt3-pixbattle-back/uploads');
+    cb(null, 'uploads');
   },
   filename: function(req, file, cb) {
     cb(null, Date.now() + '-' + file.originalname)
