cff99491072b28a0abfd2d8ec101c2bec7e23928
Benoît Hubert <benoithubert@gmail.com>
2020-07-24 07:06:32 +0200

    Fix update status for vote -> completed

--- src/helper/updateBattleStatusJobs.js
+++ src/helper/updateBattleStatusJobs.js
@@ -2,12 +2,13 @@ const CronJob = require('cron').CronJob
 const moment = require('moment')
 const connection = require('./db')
 const updateBattleStatus = require('./updateBattleStatus')
+const { votingPhase } = require('../../config')
 
 const getBattlesByStatus = (statusId, cb) => {
   const sql =
     `SELECT battle_id, deadline 
     FROM battle 
-    WHERE status_id = ? AND deadline > NOW()`
+    WHERE status_id = ?`
   connection.query(sql, statusId, (err, battles) => {
     if (err) return cb(err)
     return cb(null, battles)
@@ -25,6 +26,12 @@ getBattlesByStatus(2, (err, battles) => {
 })
 
 const scheduleStatusUpdatePostToVote = (battle) => {
+  const isPast = moment().isAfter(moment(battle.deadline))
+  if (isPast) {
+    console.warn(`Battle ${battle.battle_id} has deadline in the past: ${battle.deadline}`)
+    return
+  }
+  console.log(`Scheduling update to VOTE for battle ${battle.battle_id} @ ${battle.deadline}`)
   const job = new CronJob(battle.deadline, function () {
     updateBattleStatus(battle.battle_id, 2, err => {
       if (err) {
@@ -38,7 +45,14 @@ const scheduleStatusUpdatePostToVote = (battle) => {
 }
 
 const scheduleStatusUpdateVoteToCompleted = (battle) => {
-  const finalDate = moment(battle.deadline).add(1, 'minute')
+  const { durationNumber, durationUnit } = votingPhase
+  const finalDate = moment(battle.deadline).add(durationNumber, durationUnit)
+  const isPast = moment().isAfter(moment(finalDate))
+  if (isPast) {
+    console.warn(`Battle ${battle.battle_id} has end of voting phase in the past: ${finalDate}`)
+    return
+  }
+  console.log(`Scheduling update to COMPLETED for battle ${battle.battle_id} @ ${finalDate}`)
   const job = new CronJob(finalDate, function () {
     updateBattleStatus(battle.battle_id, 3, err => {
       if (err) {
