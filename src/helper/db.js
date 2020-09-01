c9d75a4551f7aa31b268e77b6ab35e04c97d2e9d
Benoît Hubert <benoithubert@gmail.com>
2020-09-01 18:35:27 +0200

    Close pool when Node exits

--- src/helper/db.js
+++ src/helper/db.js
@@ -2,13 +2,21 @@ const mysql = require('mysql2')
 const { dbHost, dbUser, dbPassword, dbDatabase } = require('../../config')
 const Promise = require('bluebird')
 
-const connection = mysql.createPool({
+const pool = mysql.createPool({
   host: dbHost,
   user: dbUser,
   password: dbPassword,
   database: dbDatabase
 })
 
-Promise.promisifyAll(connection)
+process.on('exit', () => {
+  console.log('About to exit, closing pool')
+  pool.end(function (err) {
+    if (err) console.error('Failed to close pool', err)
+    else console.log('Pool closed')
+  })
+})
 
-module.exports = connection
+Promise.promisifyAll(pool)
+
+module.exports = pool
